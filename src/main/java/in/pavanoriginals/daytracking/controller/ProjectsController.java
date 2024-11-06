7f9fc3c59725f1d65e3ade15ac433ee05096ad30
pavan7297 <mallapavan072@gmail.com>
2024-11-06 13:41:39 +0530

    mapping update

--- src/main/java/in/pavanoriginals/daytracking/controller/ProjectsController.java
+++ src/main/java/in/pavanoriginals/daytracking/controller/ProjectsController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import in.pavanoriginals.daytracking.model.projects;
@@ -20,6 +21,7 @@ import in.pavanoriginals.daytracking.model.projects;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/projects")
 public class ProjectsController {
 
 	@Autowired
